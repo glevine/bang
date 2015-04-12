a79e24b65b5cf15b88ab711edda2fbbdb871369b
Greg Levine <levine.greg@gmail.com>
2015-04-11 22:17:39 -0400

    Support for installing systems with fully-qualified names
    `bang user/repo/system` will look specifically for a system in `/usr/local/Galaxies/[user]/[repo]/systems/` that is named `[system].yml`. If that system does not exist, then it will fail. If a fully-qualified name is not used, then the standard practice of searching all galaxies for the first matching system will be used.

--- lib/bang/system.rb
+++ lib/bang/system.rb
@@ -2,17 +2,28 @@ require 'galaxy'
 
 module Bang
   class System
+    # Match expressions when fully-qualified system names are given as ARGS, e.g. someuser/somegalaxy/somesystem.
+    @@FQ_REGEX = %r{^([\w-]+)/([\w-]+)/([\w-]+)$}
+
     class << self
       def factory name
         p = path name
-        raise Errors::SystemUnavailableError, name unless p.file?
+        raise Errors::SystemUnavailableError, name unless p.is_a? Pathname
         System.new name, p
       end
 
       def path name
-        Galaxy.list? do |user, repo|
-          p = repo.join("systems/#{name.downcase}.yml")
+        name =~ @@FQ_REGEX
+
+        if $1 and $2 and $3
+          # it's a fully-qualified system name
+          p = Bang.galaxies?.join("#{$1.downcase}/#{$2.downcase}/systems/#{$3.downcase}.yml")
           return p if p.file?
+        else
+          Galaxy.list? do |user, repo|
+            p = repo.join("systems/#{name.downcase}.yml")
+            return p if p.file?
+          end
         end
       end
     end

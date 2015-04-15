40e1b96fa559ddbf5638e11a4594c876b6c3ab24
Greg Levine <levine.greg@gmail.com>
2015-04-15 17:13:11 -0400

    Caveats remind user to copy their SSH key for GitHub
    Also fixed a bug with the `shell` caveats when the directory did not exist.

--- lib/bang/cmd/caveats.rb
+++ lib/bang/cmd/caveats.rb
@@ -5,9 +5,18 @@ module Bang
   # Prints caveats.
   def caveats
     shell = universe?.join 'shell'
-    alert "Source the following files in your shell configuration..." if shell.children.length > 0
-    shell.children.sort.each do |file|
-      puts "#{file}" unless file.basename.to_s == '.DS_Store'
-    end if shell.directory?
+    if shell.directory?
+      alert "Source the following files in your shell configuration..." if shell.children.length > 0
+      shell.children.sort.each do |file|
+        puts "#{file}" unless file.basename.to_s == '.DS_Store'
+      end
+    end
+
+    ssh_key = Pathname.new("#{ENV['HOME']}/.ssh/id_rsa.pub")
+    if ssh_key.file?
+      alert "Add your SSH key to your GitHub account if you haven't already..."
+      puts 'Use the following command to copy your id_rsa.pub file to your clipboard:'
+      puts "$ pbcopy < #{ssh_key}"
+    end
   end
 end
